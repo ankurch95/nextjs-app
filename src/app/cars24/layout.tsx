8f207b70262acad836600ce3592406329ad56248
Ankur Chauhan <ankur0660341@gmail.com>
2024-06-20 09:15:41 +0530

    adding notification using node js

--- src/app/cars24/layout.tsx
+++ src/app/cars24/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next"
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: 'Cars24',
@@ -6,5 +7,48 @@ export const metadata: Metadata = {
 }
 
 export default function Cars24Layout({ children }: { children: React.ReactNode }) {
-    return children
+    return (
+        <html lang="en">
+            <body className="min-h-screen">
+
+                <nav className="bg-gray-200 shadow shadow-gray-300
+                 w-100 px-8 md:px-auto">
+                    <div className="md:h-16 h-28 mx-auto md:px-4
+                     container flex items-center justify-between
+                      flex-wrap md:flex-nowrap">
+                        {/* Logo */}
+                        <div className="text-indigo-500 md:order-1">
+                            <img className="h-10" src="logo.png"
+                                alt="Notifications" />
+                        </div>
+                        <div className="text-gray-500 order-3 
+                        w-full md:w-auto md:order-2">
+                            <ul className="flex font-semibold
+                                           justify-between">
+                                <li className="md:px-4 md:py-2 
+                                text-green-500">
+                                    <Link href="/">Home</Link>
+                                </li>
+                                <li className="md:px-4 md:py-2 
+                                hover:text-green-400">
+                                    <Link href="/add">
+                                        Send Notification
+                                    </Link>
+                                </li>
+
+                            </ul>
+                        </div>
+                        <div className="order-2 md:order-3">
+                            <img className="h-6" 
+                                 src="notification.png"
+                                 alt="Notifications" />
+                        </div>
+                    </div>
+                </nav>
+
+                {children}
+
+            </body>
+        </html>
+    );
 }
--- src/app/cars24/page.tsx
+++ src/app/cars24/page.tsx
@@ -6,11 +6,29 @@ import CustomDropdown from "@/component/customNavDropdown";
 import TopNavbar from "@/component/navbar";
 import { buyMenu } from "@/helpers/jsonData";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { Button, Card, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import socketio from "socket.io-client";
+
 
 export default function Cars24() {
     const [userEmail, setUserEmail] = useState<string>('')
+    const [notifications, setNotifications] = useState([]);
+
+    const socket = socketio.connect("http://localhost:4000")
+
+    useEffect(() => {
+        socket.on('connect', () => {
+            console.log(`Connected to server`);
+        })
+        socket.on('notification', (data: any) => {
+            console.log(`Notification from server`);
+            setNotifications([...notifications, data])
+        })
+        socket.on('disconnect', () => {
+            console.log(`Disconnected from server`);
+        })
+    }, [socket])
 
     return (
         <div className="min-h-screen bg-white">
@@ -135,6 +153,49 @@ export default function Cars24() {
                 </div>
                  */}
             </div>
+
+
+
+
+            <main className="grid grid-cols-2 p-24 gap-6">
+
+                {
+                    notifications ? notifications.map((notification, index) => {
+                        return (
+                            <div
+                                key={index}
+                                id="toast-message-cta"
+                                className="w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:bg-gray-800 dark:text-gray-400" role="alert">
+                                <div className="flex">
+                                    <img 
+                                    className="w-8 h-8 rounded-full"
+                                        src="notification.png" alt="Jese Leos image" />
+                                    <div className="ms-3 text-sm font-normal">
+                                        <span className="mb-1 text-sm font-semiboldtext-gray-900 dark:text-white">
+                                            {notification.name}</span>
+                                        <div className="mb-2 text-sm font-normal">
+                                            {notification.message}</div>
+                                        <a href="#" className="inline-flex px-2.5 py-1.5 text-xs font-medium text-center  text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Reply</a>
+                                    </div>
+                                    <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-white justify-center items-center flex-shrink-0 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8  dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                                        data-dismiss-target="#toast-message-cta"
+                                        aria-label="Close">
+                                        <span className="sr-only">Close</span>
+                                        <svg className="w-3 h-3" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 14 14">
+                                            <path stroke="currentColor"
+                                                strokeLinecap="round" strokeLinejoin="round"
+                                                strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    }) : null
+
+                }
+            </main>
         </div>
     )
 }
