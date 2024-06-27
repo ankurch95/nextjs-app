4b5ced86091513c032a07171118d42b09343f1f8
Ankur Chauhan <ankur0660341@gmail.com>
2024-06-27 09:23:48 +0530

    Adding video player

---
+++ src/app/videoPlayer/page.tsx
@@ -0,0 +1,45 @@
+"use client"
+import React, { useState } from 'react'
+import ReactPlayer from 'react-player'
+
+export default function videoPlayer() {
+    const [playing, setPlaying] = useState<boolean>(true);
+
+    return(
+       <div className='min-h-screen items-center justify-center flex bg-slate-600'>
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=rc23Cca2ajA"
+        playsinline={true}
+        playing={playing}
+        controls={true}
+        light={true}
+        config={{
+            youtube: {
+                playerVars: {
+                    modestbranding: 1,
+                    showinfo: 1,
+                    rel: 0,
+                    // controls: 0,
+                    autoplay: 1,
+                }
+            },
+          wistia: {
+            options: {
+              autoPlay: false,
+              controlsVisibleOnLoad: false,
+              muted: false,
+              silentAutoPlay: false,
+              volumeControl: false,
+              wmode: "transparent",
+              playPauseNotifier: false,
+            }
+          }
+        }}
+      />
+      
+</div>
+    //   <button onClick={() => setPlaying((v) => !v)}>play/pause</button>
+   
+    )
+
+}
--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -9,10 +9,10 @@ import router from "next/router";
 type Props = {
     handleShow?: () => void,
 }
-export default function TopNavbar({  handleShow }: Props) {
+export default function TopNavbar({ handleShow }: Props) {
     const [userEmail, setUserEmail] = useState<string>('')
     const [buttonDisabled, setButtonDisabled] = useState<boolean>(true)
-    
+
     useEffect(() => {
         getUserData()
     }, [])
@@ -44,23 +44,24 @@ export default function TopNavbar({  handleShow }: Props) {
         }
 
     }
-    
+
     return (
-        <Navbar  bg="light" data-bs-theme="light" className="bg-white shadow-lg justify-between px-20 py-3">
+        <Navbar bg="light" data-bs-theme="light" className="bg-white shadow-lg justify-between px-20 py-3">
             <Container >
                 {/* <Navbar.Brand onClick={handleShow} href="#home" className="ms-12">{<RxHamburgerMenu />}</Navbar.Brand> */}
                 <Nav className="me-auto">
                     <Nav.Link href="/profile" className=" text-lg">Paytm</Nav.Link>
-                     <Nav.Link href="/cars24" className=" text-lg">Cars 24</Nav.Link>
-                   <Nav.Link href="/payment" className="text-lg">Payment</Nav.Link>
-                   <Nav.Link href="/add" className="text-lg">Notification</Nav.Link>
+                    <Nav.Link href="/cars24" className=" text-lg">Cars 24</Nav.Link>
+                    <Nav.Link href="/payment" className="text-lg">Payment</Nav.Link>
+                    <Nav.Link href="/add" className="text-lg">Notification</Nav.Link>
+                    <Nav.Link href="/videoPlayer" className="text-lg">Video Player</Nav.Link>
                 </Nav>
             </Container>
 
             <Nav.Link href={`/profile/${userEmail}`} className="me-2">{userEmail}</Nav.Link>
 
-            <Nav.Link  onClick={onLogout} className="me-5">
-                <IoLogOutOutline/>
+            <Nav.Link onClick={onLogout} className="me-5">
+                <IoLogOutOutline />
             </Nav.Link>
         </Navbar>
     )
